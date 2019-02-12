f66dee4d9e0979c5011fee85794743d509587638
Shoichi Namba <alphabrend@gmail.com>
2019-02-12 19:29:42 +0900

    Add queueing

--- routes/index.js
+++ routes/index.js
@@ -1,43 +1,57 @@
 const createError = require('http-errors')
 const { launchBrowser, getPage } = require('../lib/browser')
+const async = require('async')
+
+const queue = async.queue((task, callback) => {
+  console.log(task)
+  console.log('Queue length: ' + queue.length())
+  callback()
+}, Number(process.env.QUEUE_CONCURRENCY))
 
 module.exports = async (req, res, next) => {
-  const path = req.path.replace(/\.png$/, '')
-  if (!isValidPath(path)) {
-    next()
-    return
-  }
+  queue.push({ name: req.path }, async err => {
+    if (err) {
+      console.error(err)
+      next()
+      return
+    }
+    const path = req.path.replace(/\.png$/, '')
+    if (!isValidPath(path)) {
+      next()
+      return
+    }
 
-  const browser = req.app.get('browser')
-  const url = process.env.BASE_URL + path
-  let page = null
+    const browser = req.app.get('browser')
+    const url = process.env.BASE_URL + path
+    let page = null
 
-  try {
-    page = await getPage(browser, url)
-  } catch (err) {
-    console.error(err)
-    await browser.close()
-    const newBrowser = await launchBrowser()
-    req.app.set('browser', newBrowser)
-    next(createError(500))
-    return
-  }
+    try {
+      page = await getPage(browser, url)
+    } catch (err) {
+      console.error(err)
+      await browser.close()
+      const newBrowser = await launchBrowser()
+      req.app.set('browser', newBrowser)
+      next(createError(500))
+      return
+    }
 
-  if (!page) {
-    next()
-    return
-  }
+    if (!page) {
+      next()
+      return
+    }
 
-  const img = await page.screenshot({})
-  await page.close()
+    const img = await page.screenshot({})
+    await page.close()
 
-  res.writeHead(200, {
-    'Cache-Control': 'public, max-age=14400',
-    Expires: new Date(Date.now() + 14400000).toUTCString(),
-    'Content-Type': 'image/png',
-    'Content-Length': img.length
+    res.writeHead(200, {
+      'Cache-Control': 'public, max-age=14400',
+      Expires: new Date(Date.now() + 14400000).toUTCString(),
+      'Content-Type': 'image/png',
+      'Content-Length': img.length
+    })
+    res.end(img)
   })
-  res.end(img)
 }
 
 function isValidPath(path) {
