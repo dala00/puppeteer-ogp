ec4d78c59379157cbf439702818c6a7636d7b547
Shoichi Namba <alphabrend@gmail.com>
2019-02-15 17:45:08 +0900

    Fix queue

--- routes/index.js
+++ routes/index.js
@@ -2,55 +2,55 @@ const createError = require('http-errors')
 const { launchBrowser, getPage } = require('../lib/browser')
 const async = require('async')
 
-const queue = async.queue((task, callback) => {
-  console.log(task)
+const queue = async.queue(async ({ path, req, res }) => {
+  console.log(`Task started for: ${path}`)
   console.log('Queue length: ' + queue.length())
-  callback()
+
+  const browser = req.app.get('browser')
+  const url = process.env.BASE_URL + path
+  let page = null
+
+  try {
+    page = await getPage(browser, url)
+  } catch (err) {
+    console.error(err)
+    await browser.close()
+    const newBrowser = await launchBrowser()
+    req.app.set('browser', newBrowser)
+    return err
+  }
+
+  if (!page) {
+    return 'Failed to get page'
+  }
+
+  const img = await page.screenshot({})
+  await page.close()
+
+  res.writeHead(200, {
+    'Cache-Control': 'public, max-age=14400',
+    Expires: new Date(Date.now() + 14400000).toUTCString(),
+    'Content-Type': 'image/png',
+    'Content-Length': img.length
+  })
+  res.end(img)
 }, Number(process.env.QUEUE_CONCURRENCY))
 
-module.exports = async (req, res, next) => {
-  queue.push({ name: req.path }, async err => {
+module.exports = (req, res, next) => {
+  const path = req.path.replace(/\.png$/, '')
+  if (!isValidPath(path)) {
+    next()
+    return
+  }
+
+  console.log(`Request started: ${path}`)
+  queue.push({ path, req, res }, err => {
     if (err) {
       console.error(err)
-      next()
-      return
-    }
-    const path = req.path.replace(/\.png$/, '')
-    if (!isValidPath(path)) {
-      next()
-      return
-    }
-
-    const browser = req.app.get('browser')
-    const url = process.env.BASE_URL + path
-    let page = null
-
-    try {
-      page = await getPage(browser, url)
-    } catch (err) {
-      console.error(err)
-      await browser.close()
-      const newBrowser = await launchBrowser()
-      req.app.set('browser', newBrowser)
       next(createError(500))
       return
     }
-
-    if (!page) {
-      next()
-      return
-    }
-
-    const img = await page.screenshot({})
-    await page.close()
-
-    res.writeHead(200, {
-      'Cache-Control': 'public, max-age=14400',
-      Expires: new Date(Date.now() + 14400000).toUTCString(),
-      'Content-Type': 'image/png',
-      'Content-Length': img.length
-    })
-    res.end(img)
+    console.log('Task finished')
   })
 }
 
